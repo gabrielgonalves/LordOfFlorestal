ed4410183250bf8418c69338736b464343d37427
Gabriel <gabriel.ggfreitas@gmail.com>
2017-10-09 16:59:09 -0300

    Spring Security configurado

--- LordOfFlorestal/src/main/java/br/com/lordofflorestal/web/LoginBean.java
+++ LordOfFlorestal/src/main/java/br/com/lordofflorestal/web/LoginBean.java
@@ -5,7 +5,11 @@
  */
 package br.com.lordofflorestal.web;
 
+import br.com.lordofflorestal.util.MessageUtil;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  *
@@ -13,5 +17,16 @@ import javax.faces.bean.ManagedBean;
  */
 @ManagedBean
 public class LoginBean {
+
+    private HttpServletRequest request;
+
+    public void preRender() {
+        request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+        String retorno = request.getParameter("login_error");
+        if (retorno != null) {
+            MessageUtil.erro("Houve um erro ao realizar o login. Por favor, revise suas informações");
+        }
+
+    }
 
 }
