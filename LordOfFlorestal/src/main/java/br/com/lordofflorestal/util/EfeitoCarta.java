4320ca6a29e353b21c531752fcd187482ca26e7d
Gabriel Gonçalves de Freitas <gabriel.ggfreitas@gmail.com>
2017-11-21 22:26:28 -0200

    Alteração na tela de Login, e criação de métodos para cada um dos efeitos a serem implementados

--- LordOfFlorestal/src/main/java/br/com/lordofflorestal/util/EfeitoCarta.java
+++ LordOfFlorestal/src/main/java/br/com/lordofflorestal/util/EfeitoCarta.java
@@ -10,9 +10,124 @@ package br.com.lordofflorestal.util;
  * @author gabriel
  */
 public class EfeitoCarta {
-    
-    public static void efeito (){
-        
+
+    public static void carta1() {
+
     }
-    
+
+    public static void carta3() {
+
+    }
+
+    public static void carta4() {
+
+    }
+
+    public static void carta19() {
+
+    }
+
+    public static void carta23() {
+
+    }
+
+    public static void carta46() {
+
+    }
+
+    public static void carta47() {
+
+    }
+
+    public static void carta48() {
+
+    }
+
+    public static void carta49() {
+
+    }
+
+    public static void carta50() {
+
+    }
+
+    public static void carta51() {
+
+    }
+
+    public static void carta52() {
+
+    }
+
+    public static void carta53() {
+
+    }
+
+    public static void carta54() {
+
+    }
+
+    public static void carta55() {
+
+    }
+
+    public static void carta56() {
+
+    }
+
+    public static void carta57() {
+
+    }
+
+    public static void carta58() {
+
+    }
+
+    public static void carta59() {
+
+    }
+
+    public static void carta60() {
+
+    }
+
+    public static void carta61() {
+
+    }
+
+    public static void carta62() {
+
+    }
+
+    public static void carta63() {
+
+    }
+
+    public static void carta64() {
+
+    }
+
+    public static void carta65() {
+
+    }
+
+    public static void carta66() {
+
+    }
+
+    public static void carta67() {
+
+    }
+
+    public static void carta68() {
+
+    }
+
+    public static void carta69() {
+
+    }
+
+    public static void carta70() {
+
+    }
 }
